4660bad51d1e9a1278d411dfb88807ba4234a539
xxxzerone <enpunk90@gmail.com>
2024-10-02 03:40:10 +0900

    user login security and jwt token

--- user-service/src/main/java/com/example/userservice/security/AuthenticationFilter.java
+++ user-service/src/main/java/com/example/userservice/security/AuthenticationFilter.java
@@ -1,22 +1,34 @@
 package com.example.userservice.security;
 
+import com.example.userservice.dto.UserDto;
 import com.example.userservice.service.UserService;
 import com.example.userservice.vo.RequestLogin;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Date;
 
+@Slf4j
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     private final UserService userService;
@@ -50,5 +62,25 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
                                             HttpServletResponse response,
                                             FilterChain chain,
                                             Authentication authResult) throws IOException, ServletException {
+        log.debug("successfulAuthentication username: {}", ((User) authResult.getPrincipal()).getUsername());
+        String username = ((User) authResult.getPrincipal()).getUsername();
+        UserDto userDetails = userService.getUserDetailsByEmail(username);
+
+        String secret = environment.getProperty("token.secret");
+        SecretKeySpec secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8),
+                Jwts.SIG.HS256.key().build().getAlgorithm());
+
+        Instant now = Instant.now();
+        Instant expirationTime = now.plusMillis(Long.parseLong(environment.getProperty("token.expiration_time")));
+
+        String token = Jwts.builder()
+                .subject(userDetails.getUserId())
+                .expiration(Date.from(expirationTime))
+                .issuedAt(Date.from(now))
+                .signWith(secretKey)
+                .compact();
+
+        response.addHeader("token", token);
+        response.addHeader("userId", userDetails.getUserId());
     }
 }
--- user-service/src/main/java/com/example/userservice/service/UserService.java
+++ user-service/src/main/java/com/example/userservice/service/UserService.java
@@ -13,4 +13,6 @@ public interface UserService extends UserDetailsService {
     UserDto getUserByUserId(String userId);
 
     Iterable<UserEntity> getUserByAll();
+
+    UserDto getUserDetailsByEmail(String username);
 }
